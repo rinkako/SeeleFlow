95d7e110ca1a6c941f6af6f301d3754d5af7392d
Rinkako <jiajidi@163.com>
2019-12-17 00:51:15 +0800

    Update: doc

--- src/seele/Workflow.py
+++ src/seele/Workflow.py
@@ -10,106 +10,107 @@ from enum import Enum
 
 class WorkitemStateType(Enum):
     """
-    枚举：工作项资源状态
+    Enum：Workitem resourcing status.
     """
 
     """
-    工作项创建成功，尚未资源分配
+    A workitem is created, never apply any resourcing service.
     """
     Created = 0
 
     """
-    工作项在还没执行之前就被取消了
+    A workitem is cancelled before it was fired to resource service.
     """
     Cancelled = 1
 
     """
-    工作项已经指派给某个资源，但资源还未接受
+    A workitem is allocated to a resource, but have not been accepted by the resource yet.
     """
     Allocated = 2
 
     """
-    工作项已经创建成功，但没有合适的资源可以执行
+    A workitem is fired to resource service, while resource service cannot find any resource to handle it.
     """
     BadAllocated = 3
 
     """
-    工作项已经被某个资源接受，但还未开始处理
+    A workitem is accepted by a resource, but the resource has not process it yet. 
     """
     Accepted = 4
 
     """
-    工作项正在被执行
+    A workitem is executing by a resource.
     """
     Running = 5
 
     """
-    工作项由于异常而终止
+    A workitem completed at a resource but some exceptions occurred when executing.
     """
     Exception = 6
 
     """
-    工作项在资源上自然执行完毕
+    A workitem completed at a resource.
     """
     Completed = 7
 
     """
-    工作项在资源上执行的时候被人为地设置为已经完成
+    A workitem is set to be completed at a resource by human when it was still in running state. 
     """
     ForcedCompleted = 8
 
     """
-    工作项处于待被主动拉取状态
+    A workitem is waiting for any resource to pull it and accept it.
     """
     Offered = 9
 
     """
-    工作项执行中被设置为挂起状态
+    A workitem is paused when it executing at a resource, this action is not forcible and only give a hint to resource.
     """
     Suspended = 10
 
     """
-    工作项处于未分配状态，这个状态是由Allocated状态退回后到达的状态
+    A workitem is returned from a Allocated state and wait for next transition by human.
     """
     UnOffered = 11
 
 
 class WorkQueueType(Enum):
     """
-    枚举：工作队列类型
+    Enum: Work queue type. A workitem must be in one of the listed queue without `WORKLISTED`.
+          Resource Service will move workitem cross these queues.
     """
 
     """
-    尚未资源服务的队列
+    A queue for workitems never resourcing.
     """
     UNDEFINED = 0
 
     """
-    已经处于待拉取状态的队列
+    A queue for workitems waiting for pulling and accept.
     """
     OFFERED = 1
 
     """
-    已经指派的队列
+    A queue contains all allocated workitems.
     """
     ALLOCATED = 2
 
     """
-    已经启动的队列
+    A queue contains all running workitems.
     """
     STARTED = 3
 
     """
-    挂起的队列
+    A queue contains all suspended workitems. 
     """
     SUSPENDED = 4
 
     """
-    从Offered队列退回工作项的队列
+    A queue contains all workitems that were returned from allocated state and wait for human re-allocation.
     """
     UNOFFERED = 5
 
     """
-    除了UNDEFINED队列之外的全部工作队列的透视队列（不存在实际队列）
+    A special queue which contains all workitems in all queues except `UNDEFINED`.
     """
     WORKLISTED = 6
---
+++ src/seele/resoucing/impl.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+"""
+Project Seele
+@author : Rinka
+@date   : 2019/12/17
+"""
+
+
+class ResourceService:
+    """
+    Resource Service.
+    RS is responsible for control workitems life-cycle, and provide work queue operations for participants.
+    """
+    pass
