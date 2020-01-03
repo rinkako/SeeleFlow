6653920dd0cc17b54a683c9f5de2a0ad982b57ea
Rinkako <jiajidi@163.com>
2020-01-03 23:56:51 +0800

    Update: Selector

---
+++ seele/src/main/java/org/rinka/seele/server/engine/resourcing/allocator/Allocator.java
@@ -0,0 +1,15 @@
+/*
+ * Project Seele Workflow
+ * Author : Rinka
+ * Date   : 2020/1/3
+ */
+package org.rinka.seele.server.engine.resourcing.allocator;
+
+/**
+ * Class : Allocator
+ * Usage : Base allocator for all implemented allocators.
+ *         Allocator is used to choose a participant to handle task from candidate set.
+ */
+public abstract class Allocator {
+
+}
